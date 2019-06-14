85c918cf27513a926f48a15cacd4ec3c114f1bd9
unknown <m.angel011192@gmail.com>
2019-06-14 06:09:18 +0200

    try to kill all mutants

--- src/chapter 1/print-bills.test.js
+++ src/chapter 1/print-bills.test.js
@@ -6,7 +6,9 @@ describe("print bills", () => {
     const formattedResponse = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $580.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n`;
     const formattedResponseWithLowersLimit = `Statement for BigCo\n Hamlet: $400.00 (29 seats)\n As You Like It: $357.00 (19 seats)\n Othello: $400.00 (28 seats)\nAmount owed is $1,157.00\nYou earned 3 credits\n`;
     const formattedResponseOnTheLimit = `Statement for BigCo\n Hamlet: $400.00 (30 seats)\n As You Like It: $360.00 (20 seats)\n Othello: $400.00 (30 seats)\nAmount owed is $1,160.00\nYou earned 4 credits\n`;
-    const formattedResponseOnTheLimitWithJustOne = `Statement for BigCo\n Hamlet: $400.00 (30 seats)\nAmount owed is $400.00\nYou earned 0 credits\n`;
+    const formattedResponseAudienceOnTheLimitWithJustOne = `Statement for BigCo\n Hamlet: $400.00 (30 seats)\nAmount owed is $400.00\nYou earned 0 credits\n`;
+    const formattedResponseAudienceLowerTheLimitWithJustOne = `Statement for BigCo\n Hamlet: $400.00 (29 seats)\nAmount owed is $400.00\nYou earned 0 credits\n`;
+    const formattedResponseAudienceHigherTheLimitWithJustOne = `Statement for BigCo\n Hamlet: $410.00 (31 seats)\nAmount owed is $410.00\nYou earned 1 credits\n`;
 
     it("should print the bills when the performance was high", () => {
         const invoice = invoices[0];
@@ -42,11 +44,27 @@ describe("print bills", () => {
         }
     })
 
-    it("should fails when the play equal to the limit", () => {
+    it.only("should verify when one play have the audience equal to the limit", () => {
         const invoice = invoices[4];
 
         const bills = printBills(invoice, plays);
 
-        expect(bills).toEqual(formattedResponseOnTheLimitWithJustOne);
+        expect(bills).toEqual(formattedResponseAudienceOnTheLimitWithJustOne);
+    });
+
+    it("should verify when one play have the audience lower to the limit", () => {
+        const invoice = invoices[5];
+
+        const bills = printBills(invoice, plays);
+
+        expect(bills).toEqual(formattedResponseAudienceLowerTheLimitWithJustOne);
+    })
+
+    it("should verify when one play have the audience higher to the limit", () => {
+        const invoice = invoices[6];
+
+        const bills = printBills(invoice, plays);
+
+        expect(bills).toEqual(formattedResponseAudienceHigherTheLimitWithJustOne);
     })
 })
